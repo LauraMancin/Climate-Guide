2487a2019416252100b59f03d9a02568f9ad722c
Laura Mancin <lauramancin8@gmail.com>
2026-08-19 18:22:26 -0300

    fix/arrumando a parte do pôr/nascer do sol; rajada de vento; chuva e fase da lua

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ const selecao = document.querySelector('.selecao')
 
 botaoBuscar.addEventListener("click", buscarClima);
 
+function faseLua(valor) {
+    if (valor === 0 || valor === 1) return "Lua nova";
+    if (valor < 0.25) return "Lua crescente";
+    if (valor === 0.25) return "Quarto crescente";
+    if (valor < 0.5) return "Lua crescente gibosa";
+    if (valor === 0.5) return "Lua cheia";
+    if (valor < 0.75) return "Lua minguante gibosa";
+    if (valor === 0.75) return "Quarto minguante";
+    return "Lua minguante";
+}
+
 function buscarClima() {
     
     const cidade = document.getElementById("cidade").value.trim();
@@ -57,7 +68,8 @@ function buscarClima() {
 
                 else if(descricao === 'chuva' || descricao === 'chuva leve' || descricao === 'chuva forte'){
                     body.backgroundImage = 'url(images/rain-bg.jpg)'
-                    selecao.style.backgroundColor = '#194d7079'
+                    selecao.style.backgroundColor = '#2f698f79'
+                    selecao.style.color = '#fff'
                 }
                 else if(descricao === 'neve' || descricao === 'neve leve' || descricao === 'nevasca'){
                     body.backgroundImage = 'url(images/images/cold-bg.jpg)'
@@ -76,9 +88,66 @@ function buscarClima() {
             document.getElementById("vento").textContent =
                 ventoKmH.toFixed(1);
 
-            document.getElementById("nascerSol").textContent =
-                new Date(dados.data.sunrise * 1000).toLocaleTimeString("pt-BR");
+            // Rajada de vento
+            if (dados.wind.gust) {
+                const rajadaKmH = dados.wind.gust * 3.6;
+                document.getElementById("rajada").textContent =
+                    rajadaKmH.toFixed(1);
+            } else {
+                document.getElementById("rajada").textContent =
+                    "Sem rajadas";
+            }
 
+            // Nascer e pôr do sol
+            const nascerSol = new Date(dados.sys.sunrise * 1000);
+            const porSol = new Date(dados.sys.sunset * 1000);
+
+            document.getElementById("nascerSol").textContent =
+                nascerSol.toLocaleTimeString('pt-BR', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    timeZone: 'America/Sao_Paulo'
+                });
+
+            document.getElementById("porSol").textContent =
+                porSol.toLocaleTimeString('pt-BR', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    timeZone: 'America/Sao_Paulo'
+                });
+
+            // Quantidade de chuva
+            if (dados.rain && dados.rain['1h']) {
+                document.getElementById("chuva").textContent =
+                    dados.rain['1h'] + "(última hora)";
+            } else {
+                document.getElementById("chuva").textContent =
+                    "Sem previsão de chuva ";
+            }
+
+            //api open-meteo para pegar a fase da lua
+            const urlLua = `https://api.open-meteo.com/v1/forecast?latitude=-23.5475&longitude=-46.6361&daily=moon_phase&timezone=auto`;
+
+            return fetch(urlLua);
+        })
+
+        .then(function(respostaLua) {
+
+            if (!respostaLua.ok) {
+                throw new Error("Não foi possível obter dados da lua.");
+            }
+
+            return respostaLua.json();
+        })
+
+        .then(function(dadosLua) {
+
+            console.log(dadosLua);
+
+            const faseHoje = dadosLua.daily.moon_phase[0];
+
+            document.getElementById("faseLua").textContent =
+                faseLua(faseHoje);
         })
 
         .catch(function(erro) {
