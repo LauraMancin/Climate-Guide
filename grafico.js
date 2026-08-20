ca455e58259c898765a426d4288d48f71ec33384
Laura Mancin <lauramancin8@gmail.com>
2026-08-20 14:00:54 -0300

    fix/tentando fazer o gráico aparecer na página principal

--- grafico.js
+++ grafico.js
@@ -1,10 +1,11 @@
-const ctx = document.getElementById('chart');
+const ctx = document.getElementById('graficoTemperatura');
 
 new Chart(ctx, {
 type: 'line',
 data: {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
+        
     label: 'Temperatura',
     data: [12, 19, 3, 5, 2, 3],
     borderWidth: 3,
@@ -27,6 +28,5 @@ options: {
         beginAtZero: true
     }
     },
-    
 }
 });
